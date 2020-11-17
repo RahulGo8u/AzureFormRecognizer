53fefbd9bce336f1decce44b31054e5c2624fa79
rrsimt.rahul@gmail.com <HACK@1903>
2020-11-17 14:49:07 +0400

    Code changes for Model Id change

--- FormRecognizer/Common/ConfigurationConstants.cs
+++ FormRecognizer/Common/ConfigurationConstants.cs
@@ -13,7 +13,7 @@ namespace Common
         public static string endpointURI = @"https://adcbformrecognizer.cognitiveservices.azure.com/formrecognizer/v2.1-preview.1/custom/models";
         public static string sasURI = @"https://signaturestorageacct.blob.core.windows.net/chequestorage?sv=2019-12-12&ss=b&srt=sco&st=2020-11-16T18%3A52%3A04Z&se=2020-11-17T18%3A52%3A04Z&sp=rwl&sig=bZGWoGGMEu%2FsaZWLH90incofFRtZWgN5bEFKLn4DEPQ%3D";
         public static bool trainModel = false;
-        public static string modelid = "913add5d-6361-4327-8bbd-ea4d535983d8";
+        public static string modelid = "8b22deb1-ed93-4a8c-a509-10491110a0e6";
         public static string uri = @"https://adcbformrecognizer.cognitiveservices.azure.com/formrecognizer/v2.1-preview.1/custom/models/" + modelid;
     }
 }
